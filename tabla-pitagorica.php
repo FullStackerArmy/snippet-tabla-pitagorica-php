e018a1f092c067939a7d076fe9f7f0379d4bdcd1
Franco Rios Diaz <franco.phoenix@gmail.com>
2020-04-15 14:44:23 -0500

    fix lines

--- tabla-pitagorica.php
+++ tabla-pitagorica.php
@@ -1,34 +1,29 @@
-<?php
-echo "Heeey Fullstacker!";
-?>
-
 <table>
-	<thead>
-		<tr>
-		<?php for ($column = 0; $column <= 7 ; $column++) { ?>
-			<th><?php echo $column; ?></th>
-		<?php } ?>
-		</tr>
-	</thead>
-	<tbody>
-		<?php for ($row = 1; $row <= 7 ; $row++) { ?>
-		<tr>
-			<?php
-			for ($column = 0; $column <= 7 ; $column++) {
-				$result = $column;
+  <thead>
+    <tr>
+    <?php for ($column = 0; $column <= 7 ; $column++) { ?>
+	<th><?php echo $column; ?></th>
+    <?php } ?>
+    </tr>
+  </thead>
+  <tbody>
+  <?php for ($row = 1; $row <= 7 ; $row++) { ?>
+    <tr>
+    <?php
+    for ($column = 0; $column <= 7 ; $column++) {
+      $result = $column;
 
-				if ($column === 0) {
-					$result = $row;
-				}
+      if ($column === 0) {
+        $result = $row;
+      }
 
-				if ($column > 0) {
-					$result = $column * $row;
-				}
-
-			?>
-			<td><?php echo $result; ?></td>
-			<?php } ?>
-		</tr>
-		<?php } ?>
-	</tbody>
+      if ($column > 0) {
+        $result = $column * $row;
+      }
+    ?>
+    <td><?php echo $result; ?></td>
+    <?php } ?>
+    </tr>
+  <?php } ?>
+  </tbody>
 </table>
